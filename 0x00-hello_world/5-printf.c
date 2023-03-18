8cb1f7f06a77a2ef673324514bf625658fcd37ea
sabelo23 <sabelomhlongo4@gmail.com>
2023-03-18 04:12:24 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A proogram that use a printf function
+ * Return: 0 (Pass)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
