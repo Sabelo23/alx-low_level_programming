db4da61e43c41459aa033903c32ed3efa0ac0883
sabelo23 <sabelomhlongo4@gmail.com>
2023-03-30 00:43:25 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,11 +2,13 @@
 
 /**
  * _strcat - concatenates two strings
+ * using at most n bytes from src
  * @dest: input value
  * @src: input value
- * Return: void
+ * @n: input value
+ * Return: dest
  */
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
@@ -17,13 +19,12 @@ char *_strcat(char *dest, char *src)
 	i++;
 	}
 	j = 0;
-	while (src[j] != '\0')
+	while (j < n && src[j] != '\0')
 	{
 	dest[i] = src[j];
 	i++;
 	j++;
 	}
-
 	dest[i] = '\0';
 	return (dest);
 }
