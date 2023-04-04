fdca19f6ed4b74ce9430eec61ff3db7f38edad48
sabelo23 <sabelomhlongo4@gmail.com>
2023-04-04 14:33:12 -0700

    7-print

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -5,10 +5,10 @@
  * _putchar - writes the character c to stdout
  * @c: The charater to print
  *
- * Return on success 1
+ * Return: on success 1
  * on error, -1 is required, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	return (write(1,&c, 1
+	return (write(1, &c, 1));
 }
