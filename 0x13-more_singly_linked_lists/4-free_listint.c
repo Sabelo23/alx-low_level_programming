0cf20323fbf4c8ea194812c3b8a791ae45ab0c11
sabelo23 <sabelomhlongo4@gmail.com>
2023-05-03 11:02:37 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,14 +5,16 @@
  * @head: A listint_t list that will be freed.
  *
  */
+
 void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
 	while (head)
 	{
-	temp = head->next;
-	free(head);
-	head = temp;
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
+
